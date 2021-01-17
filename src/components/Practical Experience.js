6b08b1c9468062edf1b11ff81ecbc8be9416bb2b
Jim <jimmontour@gmail.com>
2021-01-17 14:21:18 -0500

    Link components for CV sections

---
+++ src/components/Practical Experience.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Practical Experience extends Component {
+   render() {
+      return (
+         <div>
+
+         </div>
+      )
+   }
+}
