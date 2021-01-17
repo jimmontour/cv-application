573f098435a251a6ede62154fd5f6da5a7a1dabb
Jim <jimmontour@gmail.com>
2021-01-17 14:26:07 -0500

    Add logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Header() {
    return (
       <header>
+         <h1> <i class="far fa-file"></i> </h1>
          <h1>CV Application</h1>
       </header>
    )
